7d114a722cf4423804f726531a55b0b794649a35
Peter Molnar <pmo@rtfx.com>
2015-11-13 19:55:14 +0100

    Initial Commit

---
+++ BinarySearchTreeFromFile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BinarySearchTreeFromFile
+//
+//  Created by Peter Molnar on 13/11/2015.
+//  Copyright © 2015 Peter Molnar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
